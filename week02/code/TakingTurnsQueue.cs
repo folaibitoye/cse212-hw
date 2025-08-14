51d1dae614b5e4ea91c348f2dcb4479065149b1a
folaibitoye <geocomconsultancy@gamil.com>
2025-08-14 13:34:05 +0100

    Week 5 Code: Recursion

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,10 +1,10 @@
-/// <summary>
-/// This queue is circular.  When people are added via AddPerson, then they are added to the 
-/// back of the queue (per FIFO rules).  When GetNextPerson is called, the next person
-/// in the queue is saved to be returned and then they are placed back into the back of the queue.  Thus,
-/// each person stays in the queue and is given turns.  When a person is added to the queue, 
-/// a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
-/// less than they will stay in the queue forever.  If a person is out of turns then they will 
+/// <summary> 
+/// This queue is circular. When people are added via AddPerson, they are added to the 
+/// back of the queue (per FIFO rules). When GetNextPerson is called, the next person
+/// in the queue is returned and then placed back into the back of the queue. Thus,
+/// each person stays in the queue and is given turns. When a person is added to the queue, 
+/// a turns parameter is provided to identify how many turns they will be given. If the turns is 0 or
+/// less, they will stay in the queue forever. If a person is out of turns, they will 
 /// not be added back into the queue.
 /// </summary>
 public class TakingTurnsQueue
@@ -14,10 +14,8 @@ public class TakingTurnsQueue
     public int Length => _people.Length;
 
     /// <summary>
-    /// Add new people to the queue with a name and number of turns
+    /// Add new people to the queue with a name and number of turns.
     /// </summary>
-    /// <param name="name">Name of the person</param>
-    /// <param name="turns">Number of turns remaining</param>
     public void AddPerson(string name, int turns)
     {
         var person = new Person(name, turns);
@@ -27,8 +25,8 @@ public class TakingTurnsQueue
     /// <summary>
     /// Get the next person in the queue and return them. The person should
     /// go to the back of the queue again unless the turns variable shows that they 
-    /// have no more turns left.  Note that a turns value of 0 or less means the 
-    /// person has an infinite number of turns.  An error exception is thrown 
+    /// have no more turns left. A turns value of 0 or less means the 
+    /// person has an infinite number of turns. An exception is thrown 
     /// if the queue is empty.
     /// </summary>
     public Person GetNextPerson()
@@ -37,24 +35,25 @@ public class TakingTurnsQueue
         {
             throw new InvalidOperationException("No one in the queue.");
         }
-        else
+
+        Person person = _people.Dequeue();
+
+        // If turns > 0, decrement and check if they should be re-enqueued
+        if (person.Turns > 0)
         {
-            Person person = _people.Dequeue();
+            person.Turns--;
             if (person.Turns > 0)
             {
-                person.Turns -= 1;
-                if (person.Turns > 0)
-                {
-                    _people.Enqueue(person);
-                }
-            }
-            else
-            {
-                // Infinite turns (0 or negative), always re-enqueue
                 _people.Enqueue(person);
             }
-            return person;
         }
+        else
+        {
+            // Infinite turns: re-enqueue without modifying Turns
+            _people.Enqueue(person);
+        }
+
+        return person;
     }
 
     public override string ToString()
--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,7 +15,9 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        if (n <= 0)
+            return 0; // Base case
+        return n * n + SumSquaresRecursive(n - 1); // Recursive case
     }
 
     /// <summary>
@@ -40,6 +42,17 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word); // Found a permutation
+            return;
+        }
+
+        foreach (char c in letters)
+        {
+            string remaining = letters.Replace(c.ToString(), ""); // Remove chosen char
+            PermutationsChoose(results, remaining, size, word + c);
+        }
     }
 
     /// <summary>
@@ -86,6 +99,8 @@ public static class Recursion
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
+        if (remember == null)
+            remember = new Dictionary<int, decimal>();
         // Base Cases
         if (s == 0)
             return 0;
@@ -98,8 +113,11 @@ public static class Recursion
 
         // TODO Start Problem 3
 
+        if (remember.ContainsKey(s))
+            return remember[s];
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+        remember[s] = ways;
         return ways;
     }
 
@@ -119,6 +137,17 @@ public static class Recursion
     public static void WildcardBinary(string pattern, List<string> results)
     {
         // TODO Start Problem 4
+        int index = pattern.IndexOf('*');
+        if (index == -1)
+        {
+            results.Add(pattern); // No more wildcards
+            return;
+        }
+
+        // Replace * with 0
+        WildcardBinary(pattern.Substring(0, index) + "0" + pattern.Substring(index + 1), results);
+        // Replace * with 1
+        WildcardBinary(pattern.Substring(0, index) + "1" + pattern.Substring(index + 1), results);
     }
 
     /// <summary>
@@ -129,15 +158,39 @@ public static class Recursion
     {
         // If this is the first time running the function, then we need
         // to initialize the currPath list.
-        if (currPath == null) {
+        if (currPath == null)
+        {
             currPath = new List<ValueTuple<int, int>>();
         }
-        
+
         // currPath.Add((1,2)); // Use this syntax to add to the current path
 
         // TODO Start Problem 5
         // ADD CODE HERE
+        currPath.Add((x, y));
 
+        if (maze.IsEnd(x, y))
+        {
+            results.Add(currPath.AsString()); // Found solution
+            currPath.RemoveAt(currPath.Count - 1);
+            return;
+        }
+
+        // Try moving in all 4 directions
+        int[] dx = { 0, 1, 0, -1 };
+        int[] dy = { 1, 0, -1, 0 };
+
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+
+            if (maze.IsValidMove(currPath, newX, newY))
+            {
+                SolveMaze(results, maze, newX, newY, currPath);
+            }
+        }
+        currPath.RemoveAt(currPath.Count - 1); // Backtrack
         // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
     }
 }
